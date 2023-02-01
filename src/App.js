837cac32f7b06bf5d12c3bccb1399633f64bb8c8
Devang-Shah-49 <devangvshah16@gmail.com>
2023-02-01 23:21:42 +0530

    Added navbar and footer

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Navbar from './components/navbar';
+import Footer from './components/footer';
+import Login from './components/login';
+import Signup from './components/signup';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
