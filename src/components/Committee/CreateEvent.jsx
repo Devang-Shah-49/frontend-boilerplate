2c28c47ad58bb601f543b96b228fff369e919d10
Devang-Shah-49 <devangvshah16@gmail.com>
2023-02-04 14:36:55 +0530

    Create event update

--- src/components/Committee/CreateEvent.jsx
+++ src/components/Committee/CreateEvent.jsx
@@ -19,44 +19,35 @@ export default function CreateEvent() {
                   'w-full rounded-lg py-3.5 text-md font-medium leading-5 text-indigo-600',
                   'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
                   selected
-                    ? 'bg-white shadow'
+                    ? 'bg-white shadow text-bdazzledblue'
                     : 'text-blue-100 hover:bg-white/[0.12] hover:text-white'
                 )
               }>Event Registration</Tab>
             <Tab className={({ selected }) =>
                 classNames(
-                  'w-full rounded-lg py-3.5 text-sm font-medium leading-5 text-blue-700',
+                  'w-full rounded-lg py-3.5 text-md font-medium leading-5 text-indigo-600',
                   'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
                   selected
-                    ? 'bg-white shadow'
+                    ? 'bg-white shadow text-bdazzledblue'
                     : 'text-blue-100 hover:bg-white/[0.12] hover:text-white'
                 )
               }>Request Permission</Tab>
             <Tab className={({ selected }) =>
                 classNames(
-                  'w-full rounded-lg py-3.5 text-sm font-medium leading-5 text-blue-700',
+                  'w-full rounded-lg py-3.5 text-md font-medium leading-5 text-indigo-600',
                   'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
                   selected
-                    ? 'bg-white shadow'
+                    ? 'bg-white shadow text-bdazzledblue'
                     : 'text-blue-100 hover:bg-white/[0.12] hover:text-white'
                 )
               }>Check Available Venue</Tab>
         </Tab.List>
         <Tab.Panels className="mt-2">
-          <Tab.Panel className={classNames(
-                'rounded-xl bg-white p-3',
-                'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2'
-              )}>
+          <Tab.Panel>
               <CreateEventForm/>
         </Tab.Panel>
-          <Tab.Panel className={classNames(
-                'rounded-xl bg-white p-3',
-                'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2'
-              )}>Request Permission</Tab.Panel>
-          <Tab.Panel className={classNames(
-                'rounded-xl bg-white p-3',
-                'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2'
-              )}>Check Available Status</Tab.Panel>
+          <Tab.Panel >Request Permission</Tab.Panel>
+          <Tab.Panel>Check Available Status</Tab.Panel>
         </Tab.Panels>
       </Tab.Group>
         
